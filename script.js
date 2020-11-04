962397addd314402ef396c845c26c672dbbabbc6
Pournami K K <56469156+Pournami02@users.noreply.github.com>
2020-11-04 20:13:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToNewPage(){
+    var a=document.getElementById('sort').value;
+    if(a=='age')
+    parent.location='https://www.plus2net.com/';
+    else
+        alert('Please select an option');
+}
